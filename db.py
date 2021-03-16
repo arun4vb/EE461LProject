cc77768a06527a3bd5a479fd42ce6ab653d81e9c
PiercePhillips <piercephillips22@yahoo.com>
2021-03-15 19:56:01 -0500

    Added create acct/login, SHA-512 password hashing & salting

--- db.py
+++ db.py
@@ -1,18 +1,61 @@
 from pymongo import MongoClient
-import datetime
+import hashlib, uuid
 
+#establish db connection
 client = MongoClient("mongodb+srv://PiercePhillips:Seth0085@ee461l-project.lloci.mongodb.net/test_db?retryWrites=true&w=majority")
-db = client.test
-people = db.people
+db = client.EE461L_db
+#necessary collections for project
+users = db.users
+hw_sets = db.hw_sets
 
-personDocument = {
-  "name": { "first": "Alan", "last": "Turing" },
-  "birth": datetime.datetime(1912, 6, 23),
-  "death": datetime.datetime(1954, 6, 7),
-  "contribs": [ "Turing machine", "Turing test", "Turingery" ],
-  "views": 1250000
-}
+#function to process user login attempt
+#@params: user credentials to query and authenticate
+#@return: None if user not found in db, else return user data
+def login(username, password):
+    user = users.find_one({ "username": username })
 
-#people.insert_one(personDocument)
+    #user not found in db
+    if user is None:
+        return None
+    #validate user credentials
+    else:
+        if validate_password(user, password) is True:
+            return user
+        else:
+            return None
 
-print(people.find_one({ "name.last": "Turing" }))
+
+#function to create db entry for new user
+#@params: new user data to be stored
+def create_acct(username, password):
+    #make sure username is not already taken
+    if users.find_one({ 'username': username }) is None:
+        return None
+    
+    #generate salt & encrypt user password before storing in db
+    salt = uuid.uuid4().hex
+    encrypted_password = encrypt_password(password, salt)
+
+    #store user details in db
+    user_entry = {
+        "username": username,
+        "password": encrypted_password,
+        "salt": salt
+    }
+    users.insert(user_entry)
+
+#function to hash salted user password
+#@params: password and salt
+#@return: encrypted password
+def encrypt_password(password, salt):
+    input = password.encode('utf-8') + salt.encode('utf-8')
+    return hashlib.sha512(input).hexdigest()
+
+#function to determine if user-entered password matches db entry
+#@params: db-queried user data and user-entered password
+#@return: boolean result of password validation
+def validate_password(user, password):
+    if encrypt_password(password, user['salt']) == user['password']:
+        return True
+    else:
+        return False
