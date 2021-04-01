a0af722b3981bc5ff007c1b79c33dc875b967ad7
PiercePhillips <piercephillips22@yahoo.com>
2021-03-31 21:57:49 -0500

    Updated project structure, checkin/checkout Projects now have resource allocation, checkin/checkout manages this

--- final-project-api/db_driver.py
+++ final-project-api/db_driver.py
@@ -1,4 +1,7 @@
 import db
 
-print("create new project")
-proj = db.create_project('joe','coolproject3','789','this is project 3')
+#proj = db.create_project('joe', 'newproject', '111', 'description')
+print(db.checkout_hw_set('111', 'hw_set1', 20))
+print(db.checkout_hw_set('111', 'hw_set1', 20))
+print(db.checkin_hw_set('111', 'hw_set1', 20))
+print(db.checkin_hw_set('111', 'hw_set1', 20))
